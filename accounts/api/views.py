3b7f96ff8fbd558f2242d6aae224319505b1ea6b
surenlama <surenlama620@gmail.com>
2022-07-24 18:16:15 +0545

    some video removed

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,7 +1,6 @@
 from rest_framework.generics import CreateAPIView, GenericAPIView,ListAPIView
 from django.contrib.auth.models import User
 from accounts.api.serializers import (
-
     UserCreateSerialzier,
     UserListSerialzier
 
